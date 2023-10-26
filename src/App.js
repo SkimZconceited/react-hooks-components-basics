27074d0842136f126f25757cb27486e36e62e33c
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-26 15:42:32 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+// This is the main component that goes to index.js
 function App() {
   return (
     <div>
